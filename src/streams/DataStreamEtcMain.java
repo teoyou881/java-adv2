72f028f98cd1dc9946889a8a1ab1a2fd94838d81
Teo_You <teoyou881@gmail.com>
2025-03-17 22:37:24 -0400

    print, data stream

---
+++ src/streams/DataStreamEtcMain.java
@@ -0,0 +1,33 @@
+package streams;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class DataStreamEtcMain {
+
+  public static void main(String[] args) throws IOException {
+
+    FileOutputStream fos = new FileOutputStream("temp/data.dat");
+    DataOutputStream dos = new DataOutputStream(fos);
+
+    dos.writeUTF("회원A");
+    dos.writeInt(20);
+    dos.writeDouble(10.5);
+    dos.writeBoolean(true);
+    dos.close();
+
+    FileInputStream fis = new FileInputStream("temp/data.dat");
+    DataInputStream dis = new DataInputStream(fis);
+
+    //They must be taken out in order.
+    System.out.println(dis.readUTF());
+    System.out.println(dis.readInt());
+    System.out.println(dis.readDouble());
+    System.out.println(dis.readBoolean());
+    dis.close();
+  }
+
+}
